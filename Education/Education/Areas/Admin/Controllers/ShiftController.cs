5c8037333af71db010b85bf4196b5f54b1771ea6
lequangvinh0711 <lequang.vinhok@gmail.com>
2019-04-06 11:37:38 +0700

    controller ca học và học viên

---
+++ Education/Education/Areas/Admin/Controllers/ShiftController.cs
@@ -0,0 +1,48 @@
+﻿using Education.DAO;
+using Education.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Education.Areas.Admin.Controllers
+{
+    public class ShiftController : Controller
+    {
+        ShiftDAO dao = new ShiftDAO();
+        QuanLyTrungTamEntities db = new QuanLyTrungTamEntities();
+        // GET: Admin/TaiKhoan
+        public ActionResult Index()
+        {
+            var shifts = dao.GetList();
+            ViewBag.shifts = shifts;
+            return View();
+        }
+
+        public JsonResult GetShift()
+        {
+            var shifts = dao.GetList();
+            return Json(new { data = shifts }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetShiftsById(int id)
+        {
+            var shifts = dao.GetShiftById(id);
+            return Json(shifts, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Save(Shift item)
+        {
+            bool status = false;
+            dao.Save(item);
+            return new JsonResult { Data = new { status = status } };
+
+        }
+
+        public JsonResult Delete(int id)
+        {
+            var lst = dao.Delete(id);
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+	}
+}
---
+++ Education/Education/Areas/Admin/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using Education.DAO;
+using Education.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Education.Areas.Admin.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDAO dao = new StudentDAO();
+        QuanLyTrungTamEntities db = new QuanLyTrungTamEntities();
+        // GET: Admin/TaiKhoan
+        public ActionResult Index()
+        {
+            var students = dao.GetList();
+            ViewBag.students = students;
+            return View();
+        }
+
+        public JsonResult GetStudent()
+        {
+            var students = dao.GetList();
+            return Json(new { data = students }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetStudentsById(int id)
+        {
+            var students = dao.GetStudentById(id);
+            return Json(students, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult Save(Student item)
+        {
+            bool status = false;
+            dao.Save(item);
+            return new JsonResult { Data = new { status = status } };
+
+        }
+
+        public JsonResult Delete(int id)
+        {
+            var lst = dao.Delete(id);
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+	}
+}

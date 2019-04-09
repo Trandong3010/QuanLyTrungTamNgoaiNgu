e24d90a4bf1491ec016c42d60ea1559834ee5e56
Trandong.o1o0@gmail.com <trandong.o1o0@gmail.com>
2019-04-10 00:04:16 +0700

    Sua loi dang nhap

---
+++ Education/Education/Areas/Admin/Scripts/React/User.jsx
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+var contex = React.createClass {
+	render() {
+console.log("a");
+		return (
+		  <div className="shopping-list">
+			hello
+		  </div>
+		);
+			}
+}
+
+export default User;

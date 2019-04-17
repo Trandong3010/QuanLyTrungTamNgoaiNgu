cb87064faaa963f5b863033d1cb0fbd438122588
Trandong.o1o0@gmail.com <trandong.o1o0@gmail.com>
2019-04-18 00:27:29 +0700

    Cap nhật

---
+++ Education/Education/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Education.Areas.Admin.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Admin/Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

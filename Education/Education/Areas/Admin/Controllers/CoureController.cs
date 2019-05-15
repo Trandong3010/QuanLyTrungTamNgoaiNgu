b414d3406076efea64201e5f53905d1e59626e97
Trandong3010 <trandong.o1o0@gmail.com>
2019-05-15 23:52:59 +0700

    Thanh vien nhom

---
+++ Education/Education/Areas/Admin/Controllers/CoureController.cs
@@ -0,0 +1,48 @@
+﻿using Education.DAO;
+using Education.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Education.Areas.Admin.Controllers
+{
+    public class CoureController : Controller
+    {
+        // GET: Admin/Coure
+        CoureDAO dao = new CoureDAO();
+        QuanLyTrungTamEntities db = new QuanLyTrungTamEntities();
+        // GET: Admin/Staff
+        public ActionResult Index()
+        {
+            var coures = dao.GetList();
+            ViewBag.coure = coures;
+            return View();
+        }
+        public JsonResult GetCoure()
+        {
+            var coure = dao.GetList();
+            return Json(new { data = coure }, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult GetCoureById(int id)
+        {
+            var coures = dao.GetCoursesById(id);
+            return Json(coures, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Save(Course item)
+        {
+            bool status = false;
+            dao.Save(item);
+            return new JsonResult { Data = new { status = status } };
+        }
+        public JsonResult Delete(int id)
+        {
+            Course item = dao.GetCoursesById(id) as Course;
+            item.IsDelete = true;
+            dao.Save(item);
+            return Json(item, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Education/Education/DAO/CoureDAO.cs
@@ -0,0 +1,69 @@
+﻿using Education.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Education.DAO
+{
+    public class CoureDAO
+    {
+        QuanLyTrungTamEntities db = new QuanLyTrungTamEntities();
+        public Object GetList()
+        {
+            var list = db.Courses.Where(x => x.IsDelete == false).OrderBy(x => x.ID).ToList();
+            return list;
+        }
+
+
+
+        public Object GetCoursesById(int id)
+        {
+            var list = db.Courses.Where(x => x.ID == id).SingleOrDefault();
+            return list;
+        }
+
+        public void Save(Course item)
+        {
+            try
+            {
+                var old = db.Courses.Where(x => x.ID == item.ID).FirstOrDefault();
+                if (item.ID == 0)
+                {
+                    item.IsDelete = false;
+                    db.Courses.Add(item);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    if (old != null)
+                    {
+                        old.CreateTime = DateTime.Now;
+                        old.Description = item.Description;
+                        old.Name = item.Name;
+                        old.Status = item.Status;
+                        db.SaveChanges();
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public Object Delete(int id)
+        {
+            var lst = db.Courses.SingleOrDefault(x => x.IsDelete == false && x.ID == id);
+            if (lst != null)
+            {
+                lst.IsDelete = true;
+                db.SaveChanges();
+            }
+            return lst;
+        }
+    }
+}
